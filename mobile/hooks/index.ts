f017a584460ddb5d7681895a9e43dd9abea6da37
Soneca21 <soneca21@gmail.com>
2026-01-21 06:14:25 -0300

    chore: remover estrutura mobile

--- mobile/hooks/index.ts
+++
@@ -1,6 +0,0 @@
-export { supabase } from './supabaseClient';
-export { useSupabaseDashboard } from './useSupabaseDashboard';
-export { useSupabaseClients } from './useSupabaseClients';
-export { useSupabaseQuotes } from './useSupabaseQuotes';
-export { useSupabaseAgenda } from './useSupabaseAgenda';
-export { useSupabaseNotifications } from './useSupabaseNotifications';
